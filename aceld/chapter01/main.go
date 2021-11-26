3b2d05fca8ec8d720630cbda23dfc3528f4a03d0
eko.zhan <eko.z@outlook.com>
2021-11-26 09:48:20 +0800

    test: add test

--- aceld/chapter01/main.go
+++ aceld/chapter01/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"time"
+)
 
 func main() {
 	fmt.Printf("你好，世界")
+	fmt.Println(int64(time.Now().Unix()))
+	fmt.Println(int64(time.Now().Unix()))
+	fmt.Println(int64(time.Now().Unix()))
+	fmt.Println(time.Now().Nanosecond())
+	fmt.Println(time.Now().Nanosecond())
+	fmt.Println(time.Now().Nanosecond())
+	fmt.Println(time.Now().Nanosecond())
+	result, _ := rand.Int(rand.Reader, big.NewInt(100))
+	result1, _ := rand.Int(rand.Reader, big.NewInt(100))
+	result2, _ := rand.Int(rand.Reader, big.NewInt(100))
+	result3, _ := rand.Int(rand.Reader, big.NewInt(100))
+	fmt.Println(result, result1, result2, result3)
 }
